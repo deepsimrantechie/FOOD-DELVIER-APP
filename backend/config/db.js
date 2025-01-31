d09cf68e06776d51bf929c2083271ad8866547a5
SIMRAN <118015518+deepsimrantechie@users.noreply.github.com>
2025-01-31 20:50:01 +0530

    minnor chnages

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,9 @@
 import mongoose, { mongo } from "mongoose";
-//npmimport foodModel from "../models/foodModel.js";
 
 export const connectDB = async () => {
   await mongoose
     .connect(
-      "mongodb+srv://deepsimran956:Narmis28@cluster0.tiutjk5.mongodb.net/food-del"
+      "mongodb+srv://deepsimran956:fDGaDHnAnPMcropd@cluster0.ll1ha.mongodb.net"
     )
     .then(() => console.log("DB connected "));
 };
